da91c35c1a97d329ef825bcffa1052e39c7d3816
baurine <2008.hbl@gmail.com>
2018-04-20 22:25:00 +0800

    5.3 - clear code, rename 'hello_react.jsx' to 'app.jsx', remove Hello component code

--- react-demo/app/javascript/packs/app.jsx
+++ react-demo/app/javascript/packs/app.jsx
@@ -4,21 +4,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
 import MovieItem from './movie_item'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
